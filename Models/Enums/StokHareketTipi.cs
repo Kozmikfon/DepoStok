3193f3eb20538f5d97385074ba92b21bc4d9d5c9
Mahmut Baglama <mahmutbaglama98@gmail.com>
2025-07-23 17:08:11 +0300

    Stok hareket tipi işlemi stok hareketlerinde aktif hale geldi.

--- Models/Enums/StokHareketTipi.cs
+++ Models/Enums/StokHareketTipi.cs
@@ -1,11 +1,17 @@
-﻿namespace DepoStok.Models.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DepoStok.Models.Enums
 {
    
         public enum StokHareketTipi
         {
+        [Display(Name="Stok Girişi")]
             Giris = 1,
+        [Display(Name ="Stok Çıkışı")]
             Cikis = 2,
+        [Display(Name ="Transfer Girişi")]
             TransferGiris = 3,
+        [Display(Name ="Transfer Çıkışı")]
             TransferCikis = 4
         }
     
